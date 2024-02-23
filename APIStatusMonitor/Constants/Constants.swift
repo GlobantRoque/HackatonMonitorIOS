aa1e0c3766cbe77d4e2b2ca2a16251bad3f4550d
Angel Obed Olvera Borjas <angel.olvera@globant.com>
2024-02-23 14:08:55 -0600

    Services Connection

--- APIStatusMonitor/Constants/Constants.swift
+++ APIStatusMonitor/Constants/Constants.swift
@@ -9,7 +9,11 @@ struct Constants {
     static let updateInterval: Int = 300 // 5 minutes
     static let endpoints = [
         EndpointData(name: "All Characters", url: "https://hp-api.onrender.com/api/characters"),
-        EndpointData(name: "Specify Character by ID", url: "https://hp-api.onrender.com/api/character/:id")
+        EndpointData(name: "Specify Character by ID", url: "https://hp-api.onrender.com/api/character/:id"),
+        EndpointData(name: "Howards Students", url: "https://hp-api.onrender.com/api/characters/students"),
+        EndpointData(name: "Howgarts Staff", url: "https://hp-api.onrender.com/api/characters/staff"),
+        EndpointData(name: "Characters in a House", url: "https://hp-api.onrender.com/api/characters/house/gryffindor"),
+        EndpointData(name: "All Spells", url: "https://hp-api.onrender.com/api/spells")
         // Añade más endpoints según sea necesario
     ]
 }
--- APIStatusMonitor/Services/EndpointService.swift
+++ APIStatusMonitor/Services/EndpointService.swift
@@ -8,9 +8,42 @@
 import Foundation
 
 class EndpointService {
+    let apiManager = APIManager()
+    
     func fetchEndpoints(completion: @escaping ([Endpoint]) -> Void) {
-        //TODO: reemplazar con llamada a API real
-        let endpoints = Constants.endpoints.map { Endpoint(name: $0.name, url: $0.url, isActive: false) }
-        completion(endpoints)
+        let dispatchGroup = DispatchGroup()
+        var endpoints: [Endpoint] = []
+        for endpoint in Constants.endpoints {
+            dispatchGroup.enter()
+            self.apiManager.fetchData(from: endpoint.url) { success in
+                endpoints.append(Endpoint(name: endpoint.name, url: endpoint.url, isActive: success))
+                dispatchGroup.leave()
+            }
+        }
+        dispatchGroup.notify(queue: .main) {
+            completion(endpoints)
+        }
+    }
+}
+
+class APIManager {
+    func fetchData(from endpoint: String, completion: @escaping (Bool) -> Void) {
+        guard let url = URL(string: endpoint) else {
+            completion(false)
+            return
+        }
+        URLSession.shared.dataTask(with: url) { _, response, error in
+            if let _ = error {
+                completion(false)
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse else {
+                completion(false)
+                return
+            }
+            let success = (200...299).contains(httpResponse.statusCode)
+            completion(success)
+        }.resume()
     }
 }
