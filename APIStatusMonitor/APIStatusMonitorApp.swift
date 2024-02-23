e2f0642d5fed69659a6e41da8c34d5364ba6eb2f
Fidel Eduardo López Mayorga <fidel.lopez@live.com>
2024-02-23 11:55:53 -0600

    First commit

---
+++ APIStatusMonitor/APIStatusMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIStatusMonitorApp.swift
+//  APIStatusMonitor
+//
+//  Created by Fidel Eduardo López Mayorga on 23/02/24.
+//
+
+import SwiftUI
+
+@main
+struct APIStatusMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ APIStatusMonitorUITests/APIStatusMonitorUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  APIStatusMonitorUITestsLaunchTests.swift
+//  APIStatusMonitorUITests
+//
+//  Created by Fidel Eduardo López Mayorga on 23/02/24.
+//
+
+import XCTest
+
+final class APIStatusMonitorUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
